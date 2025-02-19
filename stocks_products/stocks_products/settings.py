bae029fcd8dd2420e9da7327386d092bfc66fa17
VereshaginDaniil <139614656+VereshaginDaniil@users.noreply.github.com>
2025-02-19 18:19:12 +0300

    Update settings.py

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -82,8 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
-        'HOST': '127.0.0.1',
+        'HOST': '192.168.34.120',
         'PORT': '5432',
+        'USER': 'ormadmin',
+        'PASSWORD': 'oRm2022!!!'
     }
 }
 
@@ -130,3 +132,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTERS_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
+}
