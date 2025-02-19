36a55749a52a5f7d056dcff14c73df37e7b30aa5
VereshaginDaniil <139614656+VereshaginDaniil@users.noreply.github.com>
2025-02-19 18:25:13 +0300

    Update models.py

--- stocks_products/logistic/models.py
+++ stocks_products/logistic/models.py
@@ -6,6 +6,12 @@ class Product(models.Model):
     title = models.CharField(max_length=60, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукция'
+
+    def __str__(self):
+        return self.title
 
 class Stock(models.Model):
     address = models.CharField(max_length=200, unique=True)
@@ -15,17 +21,24 @@ class Stock(models.Model):
         related_name='stocks',
     )
 
+    class Meta:
+        verbose_name = 'Склад'
+        verbose_name_plural = 'Склады'
+
+    def __str__(self):
+        return self.address
+
 
 class StockProduct(models.Model):
     stock = models.ForeignKey(
-        Stock,
+        Stock, 
         on_delete=models.CASCADE,
         related_name='positions',
     )
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
-        related_name='positions',
+        related_name='positions', 
     )
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(
@@ -33,3 +46,8 @@ class StockProduct(models.Model):
         decimal_places=2,
         validators=[MinValueValidator(0)],
     )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['stock', 'product'], name='unique stock_product')
+    ]
